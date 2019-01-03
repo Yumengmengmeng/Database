d204010a0a902013fff5c46b071875b1b81f602d
blingYYY <45956527+blingYYY@users.noreply.github.com>
2019-01-04 00:51:07 +0800

    Update positionDao.java

--- positionDao.java
+++ positionDao.java
@@ -2,6 +2,7 @@ package Position;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import Position.Position;
@@ -11,13 +12,23 @@ public class positionDao implements pDao {
 	public void Printadd(String bookNo)
 	{
 		Position posi=new Position();
-		String sql="Select bookNo,address,inLibrary From Position where bookNo=?";
+		String sql="Select bookNo,address,inLibrary From position Where bookNo=?";
 		DBUtil util=new DBUtil();
 		try {
-		Connection connection=util.getConnection();
-		PreparedStatement pStatement = connection.prepareStatement(sql);
-		pStatement.setString(1, bookNo);
-		System.out.println(posi.getBookNo()+"  "+posi.getAddress()+"  "+posi.getInLibrary());
+			Connection connection=util.getConnection();
+			PreparedStatement pStatement = connection.prepareStatement(sql);
+			ResultSet rSet;
+			pStatement.setString(1, bookNo);
+			rSet = pStatement.executeQuery();
+		while(rSet.next()){
+			posi=new Position();
+			posi.setBookNo(rSet.getString("bookNo"));
+			posi.setAddress(rSet.getString("address"));
+			posi.setInLibrary(rSet.getString("inLibrary"));	
+			System.out.println("Õº È±‡∫≈£∫"+rSet.getString("bookNo"));
+			System.out.println("Õº Èπ›≤ÿ£∫"+rSet.getString("address"));
+			System.out.println("ΩË‘ƒ◊¥Ã¨£∫"+posi.getInLibrary());
+			}
 		}catch(SQLException e) {}
 	}
 }

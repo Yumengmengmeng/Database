d4f7e8b088e15e9cdcae309635aa2a99c2f1b872
blingYYY <45956527+blingYYY@users.noreply.github.com>
2019-01-04 00:54:53 +0800

    Update readerDao.java

--- readerDao.java
+++ readerDao.java
@@ -3,6 +3,7 @@ import java.util.Properties;
 import java.sql.*;
 import javax.sql.*;
 import DBUtil.*;
+import java.util.*;
 
 public class readerDao implements rDao
 {
@@ -39,7 +40,7 @@ public class readerDao implements rDao
 	    //检查
     public boolean check(String readerNo) {
         // TODO Auto-generated method stub
-        String sql = "select readerNo,readerName,password from reader where readerName=? ";
+        String sql = "select readerNo,readerName,password from reader where readerNo=? ";
 
         DBUtil util = new DBUtil();
         Connection connection;
@@ -64,7 +65,8 @@ public class readerDao implements rDao
     }
     public void printReader(Reader reader)
     {
-    	String sql="Select readerNo,readerName,readerSex,tel,department,class From reader where readerNo=? and password=?";
+    	String sql="Select readerNo,readerName,readerSex,tel,department,class"
+    			+ " From reader where readerNo=? and password=? ";
 		DBUtil util=new DBUtil();
 		Connection connection=null;
 		PreparedStatement pStatement=null;
@@ -73,22 +75,44 @@ public class readerDao implements rDao
 			connection=util.getConnection();
 			pStatement = connection.prepareStatement(sql);
 
-            pStatement.setString(1, reader.getReaderName());
-            pStatement.setString(2, reader.getPassword());
+            pStatement.setString(1, reader.getReaderNo());
+           pStatement.setString(2, reader.getPassword());
 
             rSet = pStatement.executeQuery();
-			if(rSet.next()){
+//			System.out.println("学号："+reader.getReaderNo());
+//			System.out.println("姓名："+rSet.getString("readerName"));
+//			System.out.println("学生性别："+rSet.getString("readerSex"));
+//			System.out.println("电话："+rSet.getLong("tel"));
+//			System.out.println("系别："+rSet.getString("department"));
+//			System.out.println("学生班级："+rSet.getString("class")); 
+	
+			while(rSet.next()){
                 reader = new Reader();
                 reader.setReaderNo(rSet.getString("readerNo"));
                 reader.setReaderName(rSet.getString("readerName"));
                 reader.setReaderSex(rSet.getString("readerSex"));
-                reader.setTel(rSet.getLong("Tel"));
+                reader.setTel(rSet.getLong("tel"));
                 reader.setDepartment(rSet.getString("department"));
                 reader.setClassNo(rSet.getString("class"));
-				} 
-			System.out.println(reader.getReaderNo());
-			System.out.println(reader.getReaderName());
-			System.out.println(reader);
+			System.out.println("学号："+rSet.getString("readerNo"));
+			System.out.println("姓名："+rSet.getString("readerName"));
+			System.out.println("学生性别："+rSet.getString("readerSex"));
+			System.out.println("电话："+rSet.getLong("tel"));
+			System.out.println("系别："+rSet.getString("department"));
+			System.out.println("学生班级："+rSet.getString("class")); 
+			}  
+
+			
+
+			
+//			System.out.println("学号："+reader.getReaderNo());
+//				System.out.println("姓名："+reader.getReaderName());
+//				System.out.println("学生性别："+reader.getReaderSex());
+//				System.out.println("电话："+reader.getReaderSex());
+//				System.out.println("系别："+reader.getReaderSex());
+//				System.out.println("学生班级："+reader.getReaderSex()); 
+//				System.out.println("password："+reader.getPassword());
+			
         } catch (SQLException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();

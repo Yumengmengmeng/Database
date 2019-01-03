6cb00f182bfd0cd6cc6a68e96e9b21a75eba42fc
blingYYY <45956527+blingYYY@users.noreply.github.com>
2019-01-04 00:43:55 +0800

    Update bookTypeDao.java

--- bookTypeDao.java
+++ bookTypeDao.java
@@ -1,22 +1,35 @@
 package BookType;
 
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.ResultSet;
 
 import DBUtil.DBUtil;
 
 public class bookTypeDao implements btDao{
-	public void Printtype(String classNo)
+	public void Printtype(String number)
 	{
 		BookType booktype=new BookType();
-		String sql="Select * From Position where classNo=?";
-		DBUtil util=new DBUtil();
+		String sql="Select * From bookClass,book where (bookClass.classNo=book.classNo) and number=?";
+		ResultSet rSet=null;
 		try {
-		Connection connection=util.getConnection();
-		PreparedStatement pStatement = connection.prepareStatement(sql);
-		pStatement.setString(1, classNo);
-		System.out.println(booktype.getClassNo()+"  "+booktype.getClassName()+"  "+booktype.getClassInf());
+			PreparedStatement pStatement=null;
+			DBUtil util=new DBUtil();
+			Connection connection=util.getConnection();
+			pStatement = connection.prepareStatement(sql);
+			pStatement.setString(1, number);
+			rSet = pStatement.executeQuery();
+			while(rSet.next()){
+				booktype=new BookType();
+				booktype.setClassNo(rSet.getString("classNo"));
+				booktype.setClassName(rSet.getString("className"));
+				booktype.setClassInf(rSet.getString("classInf"));
+				System.out.println("类别编号："+rSet.getString("classNo"));
+				System.out.println("类别名称："+rSet.getString("className"));
+				System.out.println("类别介绍："+rSet.getString("classInf"));
+			}
 		}catch(SQLException e) {}
 	}
 }
